0bfc4712ef051e4a6c02a605b04768ee1da40aa0
BhawickJain <bhawick@outlook.com>
2021-05-22 21:15:58 +0100

    created event handling examples

--- react-intro-tut/workspace/my-app/src/index.js
+++ react-intro-tut/workspace/my-app/src/index.js
@@ -11,6 +11,7 @@ class Greet extends React.Component {
   constructor(props) {
     // Super allows the properties in the element to be accessible
     // and manipulated.
+    // What exactly is super () doing? Without it this.state is uninitialized
     super(props);
     // .state instantiates a state object replacing any prior state.
     // .props isn't overwritten.
@@ -77,6 +78,67 @@ class Greet extends React.Component {
 //   }
 // }
 
+// Example of using .preventDefault() and events
+function ActionLink() {
+  function handleClick(e) {
+    // If nothing is done, this prevents the event
+    // from propagating
+    // What exactly does .preventDefault() do?
+    e.preventDefault();
+    console.log("The link was clicked.");
+  }
+  return (
+    <a href="#" onClick={handleClick}>
+      Click Me
+    </a>
+  );
+}
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log("this: " + this);
+    // Why do you have to setState in this specific syntax?
+    // This is an advanced arrow func syntax for returning objects
+    // (params) => ({object: "literal"})
+    // object returned must be wrapped in ()
+    this.setState((state) => ({ isToggleOn: !state.isToggleOn }));
+  }
+
+  // // This is valid too
+  // handleClick() {
+  //   console.log("this: " + this);
+  //   // Why do you have to setState in this specific syntax?
+  //   this.setState({ isToggleOn: !this.state.isToggleOn });
+  // }
+
+  // render() {
+  //   return (
+  //     <button onClick={this.handleClick}>
+  //       {this.state.isToggleOn ? "ON" : "OFF"}
+  //     </button>
+  //   );
+
+  // Alternative to having to bind in constructor
+  // not sure why this is not recommended
+  render() {
+    return (
+      <button onClick={this.handleClick.bind(this)}>
+        {this.state.isToggleOn ? "ON" : "OFF"}
+      </button>
+    );
+  }
+}
+
+// non-functional examples of passing args to Event Handlers
+// <button onClick={(e) => this.deleteRow(id, e)}>Delete Row</button>
+// <button onClick={this.deleteRow.bind(this, id)}>Delete Row</button>
+
 // A component with no real state of its own or props can
 // be a pure function
 function App() {
@@ -86,6 +148,12 @@ function App() {
       <Greet name="Frank" />
       <Greet name="James" />
       <Greet name="Dom" surname="Yu" />
+      <div>
+        <ActionLink />
+      </div>
+      <div>
+        <Toggle />
+      </div>
     </div>
   );
 }
