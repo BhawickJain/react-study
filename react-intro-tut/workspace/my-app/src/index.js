f38b3d43b3dc91fd0871ca8163ec58244f83992f
BhawickJain <bhawick@outlook.com>
2021-05-22 14:13:53 +0100

    minimal example explaine which uses State and LifeCycle management function in a React Componentd

--- react-intro-tut/workspace/my-app/src/index.js
+++ react-intro-tut/workspace/my-app/src/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Clock extends React.Component {
+// Arrow pure function as a component
+const FormattedDate = (props) => {
+  return <span>{props.date.toLocaleTimeString()}</span>;
+};
+
+class Greet extends React.Component {
   constructor(props) {
+    // Super allows the properties in the element to be accessible
+    // and manipulated.
     super(props);
-    // Adds additional state to props
-    // You can use .state only in the constructor
-    // Everywhere else you setState to trigger ReactDOM
+    // .state instantiates a state object replacing any prior state.
+    // .props isn't overwritten.
+    // .setState only updates or adds new state key,values.
     this.state = { date: new Date() };
   }
 
@@ -29,39 +36,57 @@ class Clock extends React.Component {
     });
   }
 
-  render() {
-    return <span>{this.state.date.toLocaleTimeString()}</span>;
-  }
-}
-
-class Greet extends React.Component {
-  constructor(props) {
-    // Super allows the properties in the element to be accessible
-    // and manipulated.
-    super(props);
-    // declare to expect name property in attribute
-    this.state = { name: props.name };
+  writeName(name, surname) {
+    if (surname) {
+      return name + " " + surname;
+    } else {
+      return name;
+    }
   }
 
   render() {
+    // renders surname prop is available otherwise (||) give ""
     return (
       <div>
-        <h1>Hello, {this.state.name}!</h1>
+        <h1>Hello, {this.writeName(this.props.name, this.props.surname)}!</h1>
         <h2>
-          It is <Clock></Clock>.
+          It is <FormattedDate date={this.state.date} />
         </h2>
       </div>
     );
   }
 }
 
-ReactDOM.render(<Greet name="Nirbhey" />, document.getElementById("root"));
+// Component version
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <Greet name="Nirbhey" />
+//         <Greet name="Bhawick" />
+//         <Greet name="Dom" />
+//         <Greet name="Fran" />
+//       </div>
+//     );
+//   }
+// }
+
+// A component with no real state of its own or props can
+// be a pure function
+function App() {
+  return (
+    <div>
+      <Greet name="Nirbhey" surname="Jain" />
+      <Greet name="Fran" />
+      <Greet name="Bhawick" />
+      <Greet name="Dom" />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // EXPLANATION
-// ReactDom.render triggers the Greet component which runs its constructor. This passes
-// the name property from the element. When rendering Greet is finds that it need a the
-// Clock component. This triggers its constructor which initializes the new Date object.
-// Upon creation, componentDidMount() is triggered, which runs a timeID function with
-// setInterval. That triggers the tick() function which tells ReactDOM to watch out for
-// the date property to be changing. The tick instantiates a new Date object which holds
-// the fresh time.
+// ============
+// ReactDom.render triggers the App component which is a pure function and calls multiple Greet components. Each instance needs its constructor run. There, it passes the name property from the element. Note how this is a pure function. This triggers its constructor which takes the date property from Greet. Each Greet component manages its own time. Upon ceation, componentDidMount() is triggered, which runs a timeID holding an instance of the setInterval(). That triggers the tick() function which tells ReactDOM to watch out for the date property to be changing using .setState. The tick instantiates a new Date object which holds the fresh time. When rendering Greet is finds that it needs a the FormattedDate component. This is a pure function which takes sent in state.date and returns a span with a local time format.
+// This is an example of how data flows down from the Greet component and down to the FormattedDate component.
