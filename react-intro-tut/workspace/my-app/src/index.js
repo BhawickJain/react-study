8159360e831d4cae9c010d9eff0ac703d52cdca7
BhawickJain <bhawick@outlook.com>
2021-05-22 11:54:36 +0100

    created a Hello world component with a Time component that refreshes every second

--- react-intro-tut/workspace/my-app/src/index.js
+++ react-intro-tut/workspace/my-app/src/index.js
@@ -47,7 +47,10 @@ class Game extends React.Component {
           <Board />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>
+            {/* status */}
+            <Hello></Hello>
+          </div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
@@ -58,4 +61,34 @@ class Game extends React.Component {
 // ========================================
 // insert game component into root div
 
+// Create Hello component which uses Time component
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>
+          It is <Time></Time>.
+        </h2>
+      </div>
+    );
+  }
+}
+
+// Time component is still rendered even when declared after Hello
+class Time extends React.Component {
+  render() {
+    return <span className="time">{new Date().toLocaleTimeString()}</span>;
+  }
+}
+
+function tick() {
+  const element = <Time></Time>;
+  // ReactDOM.render(element, document.getElementById("root"));
+  ReactDOM.render(<Game />, document.getElementById("root"));
+}
+
+setInterval(tick, 1000);
+
+// Render must the last bit however!
 ReactDOM.render(<Game />, document.getElementById("root"));
