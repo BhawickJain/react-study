759f90924a712b3e680bb2b76e8c7a915ab271d2
BhawickJain <bhawick@outlook.com>
2021-05-23 13:09:48 +0100

    updated explanation and use name id attribute as keys in an unordered list

--- react-intro-concepts/workspace/my-app/src/index.js
+++ react-intro-concepts/workspace/my-app/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import Toggle from "./components/toggle.js";
 import "./index.css";
 
+// TODO move into components
+
 // Arrow pure function as a component
 const FormattedDate = (props) => {
   return <span className="time">{props.date.toLocaleTimeString()}</span>;
@@ -91,8 +93,10 @@ function ActionLink() {
 
 function ManyHello(props) {
   const Names = props.names;
-  const namesRender = Names.map(({ name, surname }) => (
-    <Greet name={name} surname={surname} />
+  const namesRender = Names.map(({ name, surname, id }) => (
+    <lu key={id}>
+      <Greet name={name} surname={surname} />
+    </lu>
   ));
 
   return (
@@ -121,18 +125,21 @@ function ManyHello(props) {
 //     );
 //   }
 // }
+
 const Names = [
-  { name: "Frank", surname: "" },
-  { name: "James", surname: "" },
-  { name: "Dom", surname: "Yu" },
-  { name: "Tom", surname: "Hanks" },
-  { name: "Kanu", surname: "Reeves" },
+  { id: 1, name: "Frank", surname: "" },
+  { id: 2, name: "James", surname: "" },
+  { id: 3, name: "Dom", surname: "Yu" },
+  { id: 4, name: "Tom", surname: "Hanks" },
+  { id: 5, name: "Kanu", surname: "Reeves" },
 ];
 
 ReactDOM.render(<ManyHello names={Names} />, document.getElementById("root"));
 
 // EXPLANATION
 // ============
-// ReactDom.render triggers the App component which is a pure function and calls multiple Greet components. Each instance needs its constructor run. There, it passes the name property from the element. Note how this is a pure function. This triggers its constructor which takes the date property from Greet. Each Greet component manages its own time. Upon ceation, componentDidMount() is triggered, which runs a timeID holding an instance of the setInterval(). That triggers the tick() function which tells ReactDOM to watch out for the date property to be changing using .setState. The tick instantiates a new Date object which holds the fresh time. When rendering Greet is finds that it needs a the FormattedDate component. This is a pure function which takes sent in state.date and returns a span with a local time format.
+// ReactDom.render triggers the ManyHello component and calls multiple Greet components. Each instance needs its own constructor run. There, it passes the Name object from the Names Array of Objects. Note how ManyHello is a pure function.
+// ManyHello takes a names property which is an array of Names Objects (name and surname), those are listed line by line. This isn't a list each name does not have key / id to help react identify them to be unique.
+// Each Greet component, triggers its own constructor which makes a the date state. Each Greet component manages its own time. Upon ceation, componentDidMount() is triggered, which runs a timeID holding an instance of the setInterval(). That triggers the tick() function which tells ReactDOM to watch out for the date property to be changing using .setState. The tick instantiates a new Date object which holds the fresh time.
+// When rendering Greet, React finds that it needs the FormattedDate component. This is a pure function which takes sent in state.date and returns a span with a local time format.
 // This is an example of how data flows down from the Greet component and down to the FormattedDate component.
-// ManyHello how takes a names property which is an array of Names Objects (name and surname), those are listed line by line. This isn't a list each name does not have key / id to help react identify them to be unique.
