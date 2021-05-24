2f80711691d99920bae2709b769c0b862d2f1622
BhawickJain <bhawick@outlook.com>
2021-05-24 21:31:05 +0100

    demonstrates an uncontrolled function and the control loop

--- react-intro-concepts/workspace/my-app/src/index.js
+++ react-intro-concepts/workspace/my-app/src/index.js
@@ -2,4 +2,30 @@ import React from "react";
 import ReactDOM, { render } from "react-dom";
 import "./index.css";
 
-ReactDOM.render(<div>Hello!</div>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    Notice how you can edit the values inside after 5 seconds. This is an
+    example of a controlled function. The onChange event is ignored to
+    demonstrate its enforcement.
+    <input
+      value="hi"
+      onChange={(e) => {
+        e.preventDefault();
+      }}
+    />
+  </div>,
+  document.getElementById("root")
+);
+
+// timeout sets the value of input to be null effectively allowing changes to the input
+setTimeout(function () {
+  ReactDOM.render(
+    <div>
+      Notice how you can edit the values inside after 5 seconds. This is an
+      example of a controlled function. The onChange event is ignored to
+      demonstrate its enforcement.
+      <input value={null} />
+    </div>,
+    document.getElementById("root")
+  );
+}, 5000);
