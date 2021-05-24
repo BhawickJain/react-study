c64175ed075246242b8a18d26563ea752b7807fa
BhawickJain <bhawick@outlook.com>
2021-05-24 08:42:49 +0100

    form example

--- react-intro-concepts/workspace/my-app/src/index.js
+++ react-intro-concepts/workspace/my-app/src/index.js
@@ -2,21 +2,48 @@ import React from "react";
 import ReactDOM, { render } from "react-dom";
 import "./index.css";
 
-function Form(props) {
-  return (
-    <form>
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-      <input type="submit" value="submit" />
-    </form>
-  );
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    // allows the state of the form to be tracked as field changes are made.
+    this.setState({ value: e.target.value });
+    console.log(this.state.value);
+  }
+
+  handleSubmit(e) {
+    alert.apply("A name was submitted: " + this.state.value);
+    // since nothing is being done with event.
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            // displayed value taken from form state
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 ReactDOM.render(
   <div>
-    <Form />
+    <NameForm />
   </div>,
   document.getElementById("root")
 );
