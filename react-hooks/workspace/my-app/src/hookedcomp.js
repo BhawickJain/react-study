3c83ff2aa1a8d48a3cd3bd446ca3bed470ce923c
BhawickJain <bhawick@outlook.com>
2021-05-27 20:11:19 +0100

    example of shared state and a custom hook

--- react-hooks/workspace/my-app/src/hookedcomp.js
+++ react-hooks/workspace/my-app/src/hookedcomp.js
@@ -2,25 +2,41 @@ import React, { useState, useEffect } from "react";
 
 // Must capitalise Example to ensure react sees them as components
 const Example = () => {
-  const [count, setCount] = useState(0);
   //useState(0) sets default value of count.
+  const [count, setCount] = useCount();
+  useDocumentTitle(`You clicked ${count} times`);
+  return (
+    <div>
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+      </div>
+      <div>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+      </div>
+    </div>
+  );
+};
 
-  //Similar to componentDidMount and ComponentDidUpdate
-  // use keywords is also recognised as a custom hook.
-  // Effect is run after changes to the DOM are made.
-  // Runs after every render, including first render.
+// this is an example of shared state
+// two buttons can mutate the count
+const useCount = () => {
+  const [count, setCount] = useState(0);
+  return [count, setCount];
+};
+
+// This is an example of a custom hook!
+// You can import the function as well
+const useDocumentTitle = (value) => {
   useEffect(() => {
     //Update the document title using the browser API
     // Must use tilda ` to enable inline args not quotes
-    document.title = `You clicked ${count} times`;
+    document.title = value;
   });
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-    </div>
-  );
+  // Use effect is similar to componentDidMount and ComponentDidUpdate
+  // use keywords is also recognised as a custom hook.
+  // Effect is run after changes to the DOM are made.
+  // Runs after every render, including first render.
 };
 
 export default Example;
