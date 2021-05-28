b588460ed53b63da2890f73f700d004baa552bc4
BhawickJain <bhawick@outlook.com>
2021-05-28 17:11:34 +0100

    rough implementation of darkmode

--- react-hooks/workspace/my-app/src/hookedcomp.js
+++ react-hooks/workspace/my-app/src/hookedcomp.js
@@ -1,9 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+
+const themes = {
+  light: {
+    background: "#f5f9e9",
+    fontColor: "#000000",
+    buttonColor: "#E6EBE0",
+  },
+  dark: {
+    background: "#1d3557",
+    fontColor: "#f1faee",
+    buttonColor: "#457b9d",
+  },
+};
+const ThemeContext = React.createContext(themes.dark);
 
 const ButtonOne = (props) => {
+  const theme = useContext(ThemeContext);
   return (
     <div>
-      <button onClick={props.onClick}>{props.name}</button>
+      <button
+        onClick={props.onClick}
+        style={{ background: theme.buttonColor, color: theme.fontColor }}
+      >
+        {props.name}
+      </button>
     </div>
   );
 };
@@ -19,25 +39,53 @@ const useDocumentTitle = (value) => {
   });
 };
 
+const Canvas = (props) => {
+  const theme = useContext(ThemeContext);
+  return (
+    <body style={{ background: theme.background, color: theme.fontColor }}>
+      {props.children}
+    </body>
+  );
+};
+
+const Title = (props) => {
+  const theme = useContext(ThemeContext);
+  return <p style={{ color: theme.fontColor }}>{props.value}</p>;
+};
+
+const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  return [darkMode, setDarkMode];
+};
+
 const Example = () => {
   const [count, setCount] = useCount();
+  const [darkMode, setDarkMode] = useDarkMode();
   useDocumentTitle(`You clicked ${count} times`);
   return (
-    <div>
-      <p>You clicked {count} times</p>
-      <ButtonOne
-        name="add"
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      />
-      <ButtonOne
-        name="minus"
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      />
-    </div>
+    <ThemeContext.Provider value={darkMode ? themes.dark : themes.light}>
+      <Canvas>
+        <Title value={`You clicked ${count} times`} />
+        <ButtonOne
+          name="add"
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        />
+        <ButtonOne
+          name="minus"
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        />
+        <ButtonOne
+          name={darkMode ? "🌖" : "🌒"}
+          onClick={() => {
+            setDarkMode(darkMode === true ? false : true);
+          }}
+        />
+      </Canvas>
+    </ThemeContext.Provider>
   );
 };
 export default Example;
--- react-hooks/workspace/my-app/src/index.js
+++ react-hooks/workspace/my-app/src/index.js
@@ -3,9 +3,4 @@ import ReactDOM, { render } from "react-dom";
 import "./index.css";
 import Example from "./hookedcomp";
 
-ReactDOM.render(
-  <div>
-    <Example></Example>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Example />, document.getElementById("root"));
